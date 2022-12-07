c4a2d34dccb55c5d813320965536a4a837def510
박환용 <phyu7776@gmail.com>
2022-12-07 21:45:14 +0900

    홈페이지 틀

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
 import axios from 'axios';
 
 function App() {
@@ -12,7 +14,35 @@ function App() {
 
     return (
         <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+            <div className="logo-bar">
+                <a href="#" className="logo">
+                    <img src="http://bnx.oa.gg/img/logo.gif" alt=""/>
+                </a>
+            </div>
+
+            <div className="menu-bar con">
+                <nav className="menu-box">
+                    <ul class="row">
+                        <li className="cell"><a href="#">BRAND</a></li>
+                        <li className="cell"><a href="#">VISUAL</a></li>
+                        <li className="cell"><a href="#">STYLE</a></li>
+                        <li className="cell"><a href="#">MEDIA</a></li>
+                        <li className="cell"><a href="#">NEWS</a></li>
+                        <li className="cell"><a href="#">STORE</a></li>
+                        <li className="cell"><a href="#">CUSTOMER</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="top-bn-box con">
+                <div className="img-box">
+                    <img src="http://bnx.oa.gg/img/20160913AM105509_3718.jpg" alt=""/>
+                </div>
+            </div>
+
+            <div>
+                백엔드에서 가져온 데이터입니다 : {hello}
+            </div>
         </div>
     );
 }
